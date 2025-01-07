a155d1137cacba497ae702901b9416e440c3254d
Radwanul Islam Nayeem <rnayeem221523@bscse.uiu.ac.bd>
2025-01-07 21:44:53 +0600

    new code added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,14 @@
 const connectDB = require('../config/db')
 
 const showAllUsers = async(req, res) => {
-     const id = 2
      const query = `SELECT *
-      FROM users
-       WHERE  id = ${id}`
+                    FROM users`
 
      connectDB.query(query, (err, results) => {
           if(err){
                console.log('fetching error: ', err);
                return res.status(500).json({ error: 'Failed to retrieve users' });
           }
-          console.log(results);
           res.status(200).json(results);
      })
 }
