4bf50fb2455eb48656ac508bf51ca9f5df0208a7
paradox-99 <rnayeem221523@bscse.uiu.ac.bd>
2026-05-29 10:51:12 +0600

    fix: update database connection configuration to remove default values for user, host, and database

--- config/db.js
+++ config/db.js
@@ -4,12 +4,14 @@ require('dotenv').config()
 
 const dbPassword = process.env.DB_PASSWORD;
 
+
 const pool = new Pool({
-     user: process.env.DB_USER || 'postgres',
-     host: process.env.DB_HOST || 'localhost',
-     database: process.env.DB_NAME || 'goCar',
+     user: process.env.DB_USER,
+     host: process.env.DB_HOST,
+     database: process.env.DB_NAME ,
      password: typeof dbPassword === 'string' ? dbPassword : '',
-     port: Number(process.env.DB_PORT || 5432)
+     port: Number(process.env.DB_PORT || 5432),
+     ssl: { rejectUnauthorized: false }
 })
 
 module.exports = pool;
